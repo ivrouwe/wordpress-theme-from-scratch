097f060f81857e2667c66f25dfc3c48dd8ae3d4e
ivrouwe <ivan.donald.vrouwe@gmail.com>
2019-11-07 13:10:40 -0700

    Create header and footer
    Create basic outline/draft of content within `header.php` and `footer.php`

--- header.php
+++ header.php
@@ -7,3 +7,21 @@
 	<?php wp_head(); ?>
 </head>
 <body>
+	<header role="banner">
+		<h1>Teamwork<b>.</b></h1>
+
+		<p><a href="#main" class="visually-hidden-skip-link">Skip To Main Content</a></p>
+
+		<p><a href="#search-the-website">Skip To Search</a></p>
+
+		<nav id="major-navigation" aria-labelledby="major-navigation-heading" role="navigation">
+			<h2 id="major-navigation-heading"><span class="visually-hidden">Major Navigation</span></h2>
+
+		</nav>
+	</header>
+
+	<?php
+
+	get_sidebar('search-the-website');
+	get_sidebar('are-you-seeking-a-job-opportunity');
+	get_sidebar('interested-in-hiring-us');
---
+++ sidebar-interested-in-hiring-us.php
@@ -0,0 +1,5 @@
+<aside id="interested-in-hiring-us" aria-labelledby="interested-in-hiring-us-heading" role="complementary">
+	<h2 class="visually-hidden" id="interested-in-hiring-us-heading">Interested in hiring us?</h2>
+
+	<p><a href="#" class="call-to-action">Get Quote</a></p>
+</aside>
